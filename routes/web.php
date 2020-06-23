de4f2f93b02faf2fa54e4bf71d91147def2bb83e
yezid00 <olanaseyezid@gmail.com>
2020-06-23 01:18:02 +0100

    modified routes

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('galleries','GalleryController');
-Route::get('/download/{file}', 'DownloadController@download');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
