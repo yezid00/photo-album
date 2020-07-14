ac9b84c9062042be45205498fca84d08677e2f97
yezid00 <olanaseyezid@gmail.com>
2020-07-14 02:25:11 +0100

    fix: fixing insecure stylesheet and script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,8 +17,8 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
-    <link href="/css/app.css" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    {{-- <link href="/css/app.css" rel="stylesheet"> --}}
 </head>
 <body>
       @include('partials.nav')  
